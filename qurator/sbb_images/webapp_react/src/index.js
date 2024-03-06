cb35ce4668d3ccd0904bce00566f385badb57479
Irina Dumitriu <Irina.Dumitriu@sbb.spk-berlin.de>
2024-03-06 13:25:48 +0100

    strict mode and root render added

--- qurator/sbb_images/webapp_react/src/index.js
+++ qurator/sbb_images/webapp_react/src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
+import { StrictMode } from 'react';
 import App from './App';
 
-ReactDOM.render(
-  
-    <App />,
-  
-  document.getElementById('root')
+const root = createRoot(document.getElementById('root'));
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
 );
 
