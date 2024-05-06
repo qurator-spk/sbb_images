d2ee2fd7263a0d9a87df1e685ca873af9cf7e4c9
Kai Labusch <Kai.Labusch@sbb.spk-berlin.de>
2024-05-06 16:58:56 +0200

    Add WZIS Meta Data Tool

---
+++ qurator/sbb_images/cli_tools/add_wzis_tags.py
@@ -0,0 +1,102 @@
+import sqlite3
+import click
+import re
+import pandas as pd
+from tqdm import tqdm
+from datetime import datetime
+
+
+@click.command()
+@click.argument('wzis-csv-file', type=click.Path(exists=True))
+@click.argument('sqlite-file', type=click.Path(exists=True))
+@click.option('--path-prefix', type=str, default=None)
+@click.option('--append', type=bool, is_flag=True, default=False)
+def cli(wzis_csv_file, sqlite_file, path_prefix, append):
+    """
+        PAGE_INFO_FILE:
+        SQLITE_FILE: sqlite3 database file that contains the images table.
+    """
+
+    timestamp = str(datetime.now())
+
+    with sqlite3.connect(sqlite_file) as conn:
+
+        try:
+            conn.execute('create table tags (id integer primary key, image_id integer, '
+                         'tag text not null, user text not null, timestamp text not null, read_only integer)')
+        except sqlite3.Error:
+            pass
+
+        try:
+            conn.execute('create index idx_tags_imageid on tags(image_id)')
+        except sqlite3.Error:
+            pass
+
+        try:
+            conn.execute('create index idx_tags_tag on tags(tag)')
+        except sqlite3.Error:
+            pass
+
+        if not append:
+            conn.execute('delete from tags where user="page-info"')
+
+        print("Reading page info ...")
+
+        df_wzis = pd.read_csv(wzis_csv_file, sep=";")
+
+        print("done")
+
+        df_wzis['path'] = \
+            df_wzis.Path.str.extract(".*?([0-9]+).*?").rename(columns={0: 'Path'}).Path + \
+            '/' + df_wzis.IDwmarks.astype(str) + '.png'
+
+        if path_prefix is not None:
+            prefix = path_prefix + '/'
+        else:
+            prefix = ''
+
+        df_wzis['fullpath'] = prefix + df_wzis.path
+
+        print("Reading image table from database ...")
+
+        df_images = pd.read_sql('select rowid,file from images', conn)
+
+        print("done.")
+
+        print("Merging WZIS meta-data ...")
+
+        df_images = df_images.merge(df_wzis, left_on='file', right_on='fullpath')
+
+        print("done.")
+
+        if len(df_images) == 0:
+            print("No matching images found. Did you use the correct path prefix?")
+            return
+
+        df_all_tags = []
+
+        author = "WZIS-Meta-Data"
+
+        for _, row in tqdm(df_images.iterrows(), total=len(df_images)):
+
+            df_all_tags.append((row.rowid, row.Refnumber, author, timestamp, 1))
+            df_all_tags.append((row.rowid, "IDMo_" + str(row.IDmotif), author, timestamp, 1))
+            df_all_tags.append((row.rowid, row.Method_de, author, timestamp, 1))
+
+            tags = [re.sub(r'\s+', '_', re.sub(r'[,()-]', ' ', s).strip())
+                    for s in row.Motiflong_de.split('/')]
+
+            for tag in tags:
+                df_all_tags.append((row.rowid, tag, author, timestamp, 1))
+
+        df_all_tags = pd.DataFrame(df_all_tags, columns=['image_id', 'tag', 'user', 'timestamp', 'read_only'])
+
+        if len(df_all_tags) == 0:
+            print("No tags added.")
+            return
+
+        df_all_tags.to_sql('tags', con=conn, if_exists='append', index=False)
+
+
+if __name__ == '__main__':
+    cli()
