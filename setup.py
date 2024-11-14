c3b8cd35db34ac7e90c4a37559aa5338b04f4028
Kai Labusch <Kai.Labusch@sbb.spk-berlin.de>
2024-11-14 12:33:30 +0100

    include package data

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     url="https://qurator.ai",
     packages=find_packages(exclude=["*.tests", "*.tests.*",
                                     "tests.*", "tests"]),
-    package_data={'': ['qurator/sbb_images/webapp/static/*']},
+    package_data={'': ['*.html', '*.js', '*.css']},
     include_package_data=True,
     install_requires=install_requires,
     entry_points={
